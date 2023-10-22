06f82a41d3bcaca2bb13afcb203a3936589c5bf2
Antonio Artimon <antonio.artimon@student.tuiasi.ro>
2023-10-22 16:05:08 +0300

    Initial commit

---
+++ app/src/main/java/com/tonio/wordapp/WordActivity.kt
@@ -0,0 +1,11 @@
+package com.tonio.wordapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
